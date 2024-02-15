4931f0222e9b2c98e29639d1ba71d519c4200371
aminRmc <ramcilovicamin@gmail.com>
2024-02-15 12:08:09 +0100

    add count

--- shifumi/my-new-project/src/components/Acceuil.jsx
+++ shifumi/my-new-project/src/components/Acceuil.jsx
@@ -6,12 +6,6 @@ import ciseaux from './../assets/image/icon-scissors.svg'
 
 export default function Acceuil() {
   return (
-    <div className=' w-[700px] gap-10 flex justify-center items-center flex-wrap'>
-      
-      <div className='cursor-pointer flex justify-center items-center bg-white h-[300px] w-[300px] rounded-[50%] border-blue-500 border-solide border-[30px]'> <img className='h-[50%] hover:h-[60%] transition-[2s]' src={feuille} alt="" /></div>
-      <div className='cursor-pointer flex justify-center items-center bg-white h-[300px] w-[300px] rounded-[50%] border-yellow-200 border-solide border-[30px]'> <img className='h-[50%] hover:h-[60%] transition-[2s]' src={ciseaux} alt="" /></div>
-      <div className='cursor-pointer flex justify-center items-center bg-white h-[300px] w-[300px] rounded-[50%] border-red-500 border-solide border-[30px]'><img className='h-[50%] hover:h-[60%] transition-[2s]' src={pierre} alt="" /> </div>
-   
-    </div>
+  
   )
 }
